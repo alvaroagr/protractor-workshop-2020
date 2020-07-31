539ae36aaeb5d0b979e856a602de650b06927604
alvaroagr <alvaro.gomez.rey@gmail.com>
2020-07-31 08:50:37 -0500

    add chrome capabilities to local config to ignore pop-ups

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -5,6 +5,13 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(0);
